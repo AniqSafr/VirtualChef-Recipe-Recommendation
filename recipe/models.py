5a84b33046791be39c9d0adef2180751f7dbabf3
Aniqsafr <211458@student.upm.edu.my>
2024-12-17 13:31:34 +0800

    create LikedRecipe Table and Edit admin Site

--- recipe/models.py
+++ recipe/models.py
@@ -23,4 +23,35 @@ class ListItem(models.Model):
 
     def __str__(self):
         return f"{self.user.username}'s item: {self.item_name}"
+    
+
+class LikedRecipe(models.Model):
+    recipe_id = models.IntegerField(primary_key=True)
+    user = models.ForeignKey(User, on_delete=models.CASCADE, related_name='liked_recipes')  # Relationship to the user
+    name = models.CharField(max_length=255)
+    author_id = models.IntegerField()
+    author_name = models.CharField(max_length=255)
+    total_time = models.CharField(max_length=50)  # Time in ISO 8601 duration format
+    date_published = models.DateField()
+    description = models.TextField()
+    images = models.URLField()
+    recipe_category = models.CharField(max_length=255)
+    keywords = models.TextField()  # Comma-separated keywords
+    recipe_ingredient_parts = models.TextField()  # Store as JSON or stringified list
+    calories = models.FloatField()
+    fat_content = models.FloatField()
+    saturated_fat_content = models.FloatField()
+    cholesterol_content = models.FloatField()
+    sodium_content = models.FloatField()
+    carbohydrate_content = models.FloatField()
+    fiber_content = models.FloatField()
+    sugar_content = models.FloatField()
+    protein_content = models.FloatField()
+    recipe_servings = models.IntegerField()
+    recipe_instructions = models.TextField()
+    recipe_ingredients = models.TextField()  # Store as JSON or stringified list
+
+    def __str__(self):
+        return f"{self.user.username}'s liked recipe: {self.name}"
+
 
